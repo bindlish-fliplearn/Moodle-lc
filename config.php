e63978f95e91e0f86c2e44b494cba422f43ff894
Gaurav Kumar <gauravkumar.inc@fliplearn.com>
2019-01-14 11:41:11 +0530

    Add config file

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'stgumsdb.fliplearn.com';
+$CFG->dbhost    = 'localhost';
 $CFG->dbname    = 'stg_moodle_lc';
 $CFG->dbuser    = 'root';
-$CFG->dbpass    = 'flip@159$$';
+$CFG->dbpass    = 'root';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
@@ -20,12 +20,20 @@ $CFG->dboptions = array (
 
 $CFG->customscripts = '/var/www/html/flip-moodleguru/customscript';
 $CFG->wwwroot   = 'http://localhost/flip_moodlelc';
-$CFG->dataroot  = '/var/www/moodledata_lc';
+$CFG->dataroot  = '/var/www/html/moodledata_lc';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
 
 require_once(__DIR__ . '/lib/setup.php');
+
+define('GURU_ANNOUNCEMENT','GURU_ANNOUNCEMENT');
+define('CONTEXT_LEVEL',50);
+define('SEND_NOTIFICATION',true);
+define('COMMUNICATION_API_URL','http://stgeventapi.fliplearn.com:8084/event/');
+define('DOMAIN_NAME','stgmoodlelc.fliplearn.com');
+define('BASE_URL','https://stgmoodlelc.fliplearn.com');
+
 
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
