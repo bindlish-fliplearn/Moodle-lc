d22f92cedf1e770248fd71997d8e6fb2d91ac450
Gaurav Kumar <gauravkumar.inc@fliplearn.com>
2019-07-30 15:19:14 +0530

    Jwplayer

---
+++ local/flipapi/lib.php
@@ -0,0 +1,30 @@
+<?php
+function jwplayerInitialize($option){
+	   global $CFG;
+	   $jwplayerKey = $option['JWPLAYER_KEY'];
+	   $path = $option['path'];
+	 	$jwplayer = "<div class='player_div' id='player_div'>
+            <div id='player'></div>
+        </div>
+        <script src= $CFG->wwwroot/repository/primecontent/pix/jwplayer-8.7.3/jwplayer.js></script>
+        <script type='text/javascript'>
+        jwplayer.key =  '$jwplayerKey';
+            var path =  '$path';
+            var playerInstance = jwplayer('player');
+                        playerInstance.setup({
+                            width: '620',
+                            height: '430',
+                            bufferlength: '1',
+                            controlbar: 'none',
+                            stretching: 'uniform',
+                            autostart: 'true',
+                            primary: 'flash',
+                            hlshtml: true,
+                            file: path,
+                            defaultBandwidthEstimate : 240000
+                        });
+        </script>";
+		return $jwplayer;
+} 
+
+?>
--- local/flipapi/version.php
+++ local/flipapi/version.php
@@ -13,7 +13,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 
-$plugin->version  = 2011101333;  
+$plugin->version  = 2011101334;  
  // The (date) version of this module + 2 extra digital for daily versions
                                   // This version number is displayed into /admin/forms.php
                                   // TODO: if ever this plugin get branched, the old branch number
